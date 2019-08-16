715c56213b222328985b664fabeeef239eb798cb
shawnyhw6n9 <sunkistfish@hotmail.com>
2019-08-16 15:42:36 +0800

    Update practice command

--- lab1/practice.sh
+++ lab1/practice.sh
@@ -1,5 +1,15 @@
 docker build -t my/image1:myTag -f ./Dockerfile1 . 
 docker images --filter=reference='my/*'
-docker run --rm my/image1:myTag
+docker run --rm --name myContainer my/image1:myTag
 docker ps -a 
-docker exec -it  my/image1:myTag sh
+docker exec -it myContainer sh
+docker exec -it myContainer ls -la output.txt
+docker stop myContainer
+
+docker build -t my/image2:myTag -f ./Dockerfile2 . 
+
+docker images --filter=reference='my/image1*' -q
+docker tag  <<image_id>> <<dockerhub-id>>/image1:myTag 
+docker login
+docker push <<dockerhub-id>>/image1:myTag
+docker pull <<dockerhub-id>>/image1:myTag
--- lab3/practice.sh
+++ lab3/practice.sh
@@ -1,5 +1,9 @@
 docker run -d -it --rm --name test1 -p 8080:80 nginx:latest
 
+docker build -t my/image3:myTag -f ./Dockerfile3 . 
+docker run -it --rm -p 80 my/image3:myTag
+docker ps
+
 docker run -it --rm --name test2 --net isolated_nw -h test2 busybox sh
 / # ip a
 
@@ -7,5 +11,3 @@ docker exec -it test2 ip a
 
 
 docker run -it --rm --name test3 --net isolated_nw busybox ping test2
-
-docker build -t my/image1:myTag -f ./Dockerfile1 . 
