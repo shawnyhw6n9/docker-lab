e4055203c0ed2e4dc93eb3e86db3bbd2a99b44a8
shawnyhw6n9 <sunkistfish@hotmail.com>
2019-08-21 10:45:45 +0800

    Revert rename file

--- lab4/practice.sh
+++ lab4/practice.sh
@@ -1,10 +1,10 @@
-docker-compose -f docker-compose-1.yml build
+docker-compose -f docker-compose-1.yaml build
 docker images
 
-docker-compose -f docker-compose-1.yml up -d
+docker-compose -f docker-compose-1.yaml up -d
 
-docker-compose -f docker-compose-1.yml ps
+docker-compose -f docker-compose-1.yaml ps
 
 docker logs <<container id>>
 
-docker-compose -f /docker-lab/lab4/docker-compose-2.yml --project-directory /docker-lab/lab3/exam1 ps
+docker-compose -f ~/docker-lab/lab4/docker-compose-2.yaml --project-directory ~/docker-lab/lab3/exam1 ps
