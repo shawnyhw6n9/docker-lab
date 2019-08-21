96cb892b78dc4fc4e6972b72c81e3fbe81e4e9ca
shawnyhw6n9 <sunkistfish@hotmail.com>
2019-08-21 10:33:37 +0800

    rename filename

--- lab3/practice.sh
+++ lab3/practice.sh
@@ -4,6 +4,8 @@ docker build -t my/image4:myTag -f ./Dockerfile4 .
 docker run -it --rm -p 80 my/image4:myTag
 docker ps
 
+docker network create --driver bridge isolated_nw
+
 docker run -it --rm --name test2 --net isolated_nw -h test2 busybox sh
 / # ip a
 
--- lab4/practice.sh
+++ lab4/practice.sh
@@ -1,10 +1,10 @@
-docker-compose -f docker-compose.yml build
+docker-compose -f docker-compose-1.yml build
 docker images
 
-docker-compose -f docker-compose.yml up -d
+docker-compose -f docker-compose-1.yml up -d
 
-docker-compose -f docker-compose.yml ps
+docker-compose -f docker-compose-1.yml ps
 
 docker logs <<container id>>
 
-docker-compose -f /docker-lab/lab4/docker-compose-2.yaml --project-directory /docker-lab/lab3/exam1 ps
+docker-compose -f /docker-lab/lab4/docker-compose-2.yml --project-directory /docker-lab/lab3/exam1 ps
