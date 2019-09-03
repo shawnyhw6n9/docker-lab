18c54f940e8a1044720198a95954b40ad43b2193
shawnyhw6n9 <sunkistfish@hotmail.com>
2019-09-03 13:58:04 +0800

    Update file path

--- lab3/exam1/run.sh
+++ lab3/exam1/run.sh
@@ -1,8 +1,8 @@
 #下載 ROOT.war
 
-docker run -d -it --name ap -p 8081:8080 --net isolated_nw -h ap -v /docker-lab/lab3/exam1/ap:/var/lib/jetty/webapps jetty:jre8-alpine
+docker run -d -it --name ap -p 8081:8080 --net isolated_nw -h ap -v /Users/shawnyhw6n9/Documents/study/east\ and\ west/Fins_Digital_Banking_training/3_Container\ training/_LAB/docker-lab/lab3/exam1/ap:/var/lib/jetty/webapps jetty:jre8-alpine
 
-docker run -d -it --name web -p 8080:80 --net isolated_nw -h web --link ap -v /docker-lab/lab3/exam1/web:/etc/nginx/conf.d nginx:stable-alpine
+docker run -d -it --name web -p 8080:80 --net isolated_nw -h web --link ap -v /Users/shawnyhw6n9/Documents/study/east\ and\ west/Fins_Digital_Banking_training/3_Container\ training/_LAB/docker-lab/lab3/exam1/web:/etc/nginx/conf.d nginx:stable-alpine
 
 # 或 shawnyhw6n9/custom-nginx:dev
 
